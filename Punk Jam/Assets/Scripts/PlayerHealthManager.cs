30b29166c80f12989f27338f65fc682d65327c3c
rakimkz1 <rahimibat14@gmail.com>
2025-11-29 12:11:59 +0500

    fghbiujok

--- Punk Jam/Assets/Scripts/PlayerHealthManager.cs
+++ Punk Jam/Assets/Scripts/PlayerHealthManager.cs
@@ -1,10 +1,12 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealthManager : MonoBehaviour
 {
     public float health;
     public float maxHealth;
+    public Image healthBar;
 
     public void TakeDamage(float damage)
     {
